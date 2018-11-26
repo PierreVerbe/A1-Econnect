a8aa677cee7bc76a1b8300a84955be3f9f42fd35
pablogra <pablo.grana@isep.fr>
2018-11-26 11:28:25 +0100

    ajout acces maison

--- Econnect/Vues/Administrateur/acces_maison.php
+++ Econnect/Vues/Administrateur/acces_maison.php
@@ -1,9 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style_admin.css">
+		<title>Accès maison</title>
+	</head>
 
-</body>
+	<body>
+
+		<?php include("header_admin.php")?>
+
+		<section>
+
+			<div id="confirmation">
+
+				<h1>Accès maison du client</h1>
+
+				<label>N° Client : </label><input type="text" name="numero_client" />
+
+				<p>J'atteste sur l'honneur du propriétaire de la maison pour accéder à la gestion de cette dernière</p>
+
+				<input type="checkbox" name="confirm"><br />
+
+				<input type="button" name="validate" value="Valider">
+			</div>
+
+
+		</section>
+
+		<?php include("footer_admin.php")?>
+
+	</body>
+
 </html>
