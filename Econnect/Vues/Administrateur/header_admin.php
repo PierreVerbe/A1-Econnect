cdde22cadda0f9ecf4a7cf5f312e563323e745dd
pablogra <pablo.grana@isep.fr>
2018-12-03 10:52:07 +0100

    bug

--- Econnect/Vues/Administrateur/header_admin.php
+++ Econnect/Vues/Administrateur/header_admin.php
@@ -11,7 +11,7 @@
 			<img src="../Image/Econnect_feuille_mini.png" alt="Logo_Econnect"/>
 			<div id = "Menu">
 				<ul>
-					<li><a href="accueil_admin.php">Accueil</a></li>
+					<li><a href="accueil_admin.php">Accueil.</a></li>
 					<li><a href="liste_client.php">Liste des clients</a></li>
 					<li><a href="acces_maison.php">Accès maison</a></li>
 					<li><a href="chat_sav.php">Chat/SAV</a></li>
