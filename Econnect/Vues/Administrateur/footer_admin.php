27365e9744c04c3624b1bf3a3482d47b47548b7b
pablogra <pablo.grana@isep.fr>
2018-12-03 09:48:23 +0100

    uniformisation footer admin et client

--- Econnect/Vues/Administrateur/footer_admin.php
+++ Econnect/Vues/Administrateur/footer_admin.php
@@ -13,46 +13,39 @@
             <p Id="a_propos">
                 <b>A PROPOS:</b> <br />
     
-                <img src="../Icons/Confidentialite.png" alt="Icon confidentialité" width="50" height="50">
-                Politique de confidentialité<br />
+                <img src="../Icons/Confidentialite.png" alt="Icon confidentialité" width="35" height="35"><a classe="qsdjsd" href="../Client/confidentialite_client.php">
+                Politique de confidentialité</a> <br />
 
-                <img src="../Icons/Copyright.png" alt="Icon Copyright" width="50" height="50">
-                Copyright Econnect ©<br />
+                <img src="../Icons/Copyright.png" alt="Icon Copyright" width="35" height="35"><a classe="qsdjsd" href="../Client/copyright_client.php">
+                Copyright Econnect©</a> <br />
             </p>
 
             <p id="le_groupe" >
                 <b>LE GROUPE ECONNECT:</b> <br />
 
-                <img src="../Icons/Interrogation.png" alt="Icon interrogation" width="50" height="50"><a classe="qsdjsd" href="quiSommesNous.php">
+                <img src="../Icons/Interrogation.png" alt="Icon interrogation" width="35" height="35"><a classe="qsdjsd" href="../Client/quiSommesNous_client.php">
                 Qui sommes nous ?</a> <br />
 
+                <img src="../Icons/Valeurs.png" alt="Icon valeur" width="35" height="35"><a classe="qsdjsd" href="../Client/nosValeurs_client.php">
+                Nos valeurs</a> <br />
             </p>
 
             <p id="nous_contacter">
                 <b>NOUS CONTACTER:</b> <br />
 
-                <img src="../Icons/Epingle.png" alt="Icon epingle" width="50" height="50">
-                Localisation <br />
-
-                <img src="../Icons/Mail.png" alt="Icon mail" width="50" height="50">
-                Adresse mail<br />
-
-                <img src="../Icons/Telephone.png" alt="Icon téléphone" width="50" height="50">
-                Téléphone<br />
+                <img src="../Icons/Epingle.png" alt="Icon epingle" width="35" height="35"><a classe="qsdjsd" href="../Client/informations_client.php">
+                Informations</a> <br />
             </p>
   
             <p id="aide">
                 <b>AIDE:</b> <br />
 
-                <img src="../Icons/Aide.png" alt="Icon aide" width="50" height="50">
-                Besoin d'aide ?<br />
+                <img src="../Icons/Aide.png" alt="Icon aide" width="35" height="35"><a classe="qsdjsd" href="../Client/aides_client.php">
+                Besoin d'aide ?</a> <br />
 
-                <img src="../Icons/SAV.png" alt="Icon SAV" width="50" height="50">
-                SAV<br />
+                <img src="../Icons/SAV.png" alt="Icon SAV" width="35" height="35"><a classe="qsdjsd" href="../Client/SAV_client.php">
+                SAV</a> <br />
             </p>
-
         </footer>
-  
     </body>
-
 </html>
