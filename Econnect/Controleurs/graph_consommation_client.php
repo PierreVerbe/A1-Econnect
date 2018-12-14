6ffe69cdf12ad81596c44a3a63f9fe000707991f
Pierre Verbe <pierre.verbe@free.fr>
2018-12-14 19:25:57 +0100

    graph conso

--- Econnect/Controleurs/graph_consommation_client.php
+++ Econnect/Controleurs/graph_consommation_client.php
@@ -2,9 +2,7 @@
 require_once ('../jpgraph-4.2.5/src/jpgraph.php');
 require_once ('../jpgraph-4.2.5/src/jpgraph_bar.php');
 
-$data1y=array(575,650,720,800);
-
-
+$data1y=array(900,1100,1950,2100,2200);
 
 // Create the graph. These two calls are always required
 $graph = new Graph(700,400,'auto');
@@ -13,11 +11,13 @@ $graph->SetScale("textlin");
 $theme_class=new UniversalTheme;
 $graph->SetTheme($theme_class);
 
-$graph->yaxis->SetTickPositions(array(0,200,400,600,800), array(100,300,400,500,700));
+$graph->yaxis->SetTickPositions(array(0,500,1000,1500,2000,2500));
+$graph->yaxis->title->Set("Consommation en KWh");
 $graph->SetBox(false);
 
 $graph->ygrid->SetFill(false);
-$graph->xaxis->SetTickLabels(array('Sep.','Oct.','Nov.','Déc.'));
+$graph->xaxis->SetTickLabels(array('Aout','Sep.','Oct.','Nov.','Déc.'));
+$graph->xaxis->title->Set("Mois de l'année");
 $graph->yaxis->HideLine(false);
 $graph->yaxis->HideTicks(false,false);
 
@@ -27,11 +27,10 @@ $b1plot = new BarPlot($data1y);
 // ...and add it to the graPH
 $graph->Add($b1plot);
 
-
 $b1plot->SetColor("white");
 $b1plot->SetFillColor("#cc1111");
 
-$graph->title->Set("Consommation en kWh");
+$graph->title->Set("Consommation électrique des 5 derniers mois");
 
 // Display the graph
 $graph->Stroke();
