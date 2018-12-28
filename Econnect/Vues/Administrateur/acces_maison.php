c0fa27b497b07a0ce25ca694f4bbf285cd5e9147
pablogra <pablo.grana@isep.fr>
2018-12-28 14:53:51 +0100

    ajout javascript accès maison

--- Econnect/Vues/Administrateur/acces_maison.php
+++ Econnect/Vues/Administrateur/acces_maison.php
@@ -16,13 +16,32 @@
 
 				<h1>Accès maison du client</h1>
 
-				<label>N° Client : </label><input type="text" name="numero_client" />
+				<form name="access_home" action="http://localhost/Econnect/A1-Econnect/Econnect/Vues/Client/accueil_client.php" onsubmit="return validateForm()" method="post">
+					
+					<label>N° Client : </label><input type="text" name="numero_client" />
 
-				<p>J'atteste sur l'honneur du propriétaire de la maison pour accéder à la gestion de cette dernière</p>
+					<p>J'atteste sur l'honneur du propriétaire de la maison pour accéder à la gestion de cette dernière</p>
 
-				<input type="checkbox" name="confirm"><br />
+					<input type="checkbox" name="confirm"><br />
 
-				<input type="button" name="validate" value="Valider">
+					<input type="submit" name="validate" value="Valider">
+
+				</form>
+
+				<script type="text/javascript">
+					
+					function validateForm(){
+						if (document.forms["access_home"]["numero_client"].value != "" && document.forms["access_home"]["confirm"].checked == true){
+							document.location.href="http://localhost/Econnect/A1-Econnect/Econnect/Vues/Client/accueil_client.php"
+						}
+						else{
+							alert("!");
+							return false;
+						}
+					}
+				</script>
+
+				
 			</div>
 
 
