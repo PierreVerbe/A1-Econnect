c72eea60b55b56cb08d3d50b0b251f6bb4c68c97
Pierre Verbe <pierre.verbe@free.fr>
2019-01-07 10:35:13 +0100

    Jquery sur le contenu des tickets Page SAV client

--- Econnect/Vues/Client/SAV_client.php
+++ Econnect/Vues/Client/SAV_client.php
@@ -1,8 +1,6 @@
 <?php include ("header_client.php")?>
 <?php include ("banniere_client.php")?>
 
-<body>
-
 	<section class="bloc_SAV">
 
 		<h1> Chat/SAV</h1>
@@ -16,15 +14,13 @@
 
 		<script src="http://code.jquery.com/jquery.min.js"></script>
 			<script>
-				var table = document.getElementById('SAV_table');
-
+				var table = document.getElementById('SAV_table_client');
+				//alert(table);
 				for(var i = 1; i < table.rows.length; i++){
 					table.rows[i].onclick = function(){
 						var id_ticket = this.cells[0].innerHTML;
-						//test
-						//document.getElementById("contenu_ticket").innerHTML = 5 + 6;
-						//fin test
-						document.getElementById('num_ticket').value = id_ticket;
+						//document.getElementById('num_ticket').value = id_ticket;
+						
 						$.post("../../Modeles/Client/Requete_SAV_Contenu_Ticket.php", {postid: id_ticket},
 							function(data){
 								$('#contenu_ticket').html(data);
@@ -37,11 +33,6 @@
 		<h2>Contenu du ticket :</h2>
 
 		<div id="contenu_ticket"></div>
-
-		<p></p>
-
-		<!-- Pour la barre de délimitation -->
-		<hr width=90% align =center>
 
 		<h2>Ouvrir un nouveau ticket :</h2>
 
