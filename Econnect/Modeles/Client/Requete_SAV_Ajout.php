325e08d35df56c1bdbe29d910729b6bfaa9b2b80
Yanis ISEP <yanis.kettou@isep.fr>
2019-01-11 10:35:01 +0100

    mdp change

--- Econnect/Modeles/Client/Requete_SAV_Ajout.php
+++ Econnect/Modeles/Client/Requete_SAV_Ajout.php
@@ -1,7 +1,7 @@
 <?php
 
 	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=econnect_v2;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$bdd = new PDO('mysql:host=localhost;dbname=econnect_v2;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}
 	catch (Exception $e){
 		die('Erreur : ' . $e->getMessage());
