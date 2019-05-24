699cd90c6837e84bf7ca37b74cb84d8c744619e9
Pierre Verbe <pierre.verbe@free.fr>
2019-05-24 10:19:24 +0200

    première page température dynamique

--- Econnect/Modeles/Client/bdd_get-rooms-temp_client.php
+++ Econnect/Modeles/Client/bdd_get-rooms-temp_client.php
@@ -9,7 +9,10 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 
-	$req = $bdd->query("SELECT * FROM piece INNER JOIN user_maison ON piece.ID_Maison = user_maison.ID_Maison INNER JOIN utilisateur ON user_maison.ID_User = utilisateur.ID_User WHERE Adresse_email = \"pablo.grana@isep.fr\";");	
+	$req = $bdd->prepare("SELECT * FROM piece INNER JOIN user_maison ON piece.ID_Maison = user_maison.ID_Maison INNER JOIN utilisateur ON user_maison.ID_User = utilisateur.ID_User WHERE utilisateur.ID_User = ?");
+	$req->bindParam(1, $_SESSION['id']);
+	$req->execute();
+	//$req = $bdd->query("SELECT * FROM piece INNER JOIN user_maison ON piece.ID_Maison = user_maison.ID_Maison INNER JOIN utilisateur ON user_maison.ID_User = utilisateur.ID_User WHERE ID_User = ".$_session['id'].";");	
 
 	while ($donnees = $req->fetch())
 	{
