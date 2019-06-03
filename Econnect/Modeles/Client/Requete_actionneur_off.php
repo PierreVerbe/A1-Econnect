d75ee1b30150f893c8a28bb7da5c1226653143d6
pablogra <pablo.grana@isep.fr>
2019-06-03 17:54:07 +0200

    ajout commande actionneur

---
+++ Econnect/Modeles/Client/Requete_actionneur_off.php
@@ -0,0 +1,17 @@
+<?php
+  // Allumage de l'actionneur
+  $trame = "1G02D2a01000021";
+  $adresse = "http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G02D&TRAME=".$trame;
+  echo $adresse;
+  $ch = curl_init();
+  curl_setopt(
+    $ch,
+    CURLOPT_URL,
+    $adresse
+  );
+  curl_setopt($ch, CURLOPT_HEADER, FALSE);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	$data = curl_exec($ch);
+  curl_close($ch);
+
+?>
--- Econnect/Modeles/Client/setEtat.php
+++ Econnect/Modeles/Client/setEtat.php
@@ -12,6 +12,14 @@ $actionneur = htmlspecialchars($_GET["actionneur"]);
 
 $setEtat = $bdd->query('UPDATE actionneur SET ETAT_Actionneur = '.$etat.' WHERE ID_Actionneur = '.$actionneur.'');
 
+if ($etat == 1)
+{
+	include ("Requete_actionneur_on.php");
+}
+else if ($etat == 0)
+{
+	include ("Requete_actionneur_off.php");
+}
 
 header('Location: ../../Vues/Client/parametre_client.php');
 
