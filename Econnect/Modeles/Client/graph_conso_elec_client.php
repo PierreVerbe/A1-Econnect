a8826a746d7d09fcd24c2a6d622905557fc1b4fb
Pierre Verbe <pierre.verbe@free.fr>
2019-06-03 17:49:07 +0200

    pas de data accueil

--- Econnect/Modeles/Client/graph_conso_elec_client.php
+++ Econnect/Modeles/Client/graph_conso_elec_client.php
@@ -1,82 +1,83 @@
 <?php // content="text/plain; charset=utf-8"
-require_once ('../../jpgraph-4.2.5/src/jpgraph.php');
-require_once ('../../jpgraph-4.2.5/src/jpgraph_line.php');
+	require_once ('../../jpgraph-4.2.5/src/jpgraph.php');
+	require_once ('../../jpgraph-4.2.5/src/jpgraph_line.php');
 
-try
-{
-	include ("../../Modeles/Requete_parametre.php");
-}
-catch (Exception $e)
-{
-	die('Erreur : ' . $e->getMessage());
-}
+	try{
+		include ("../../Modeles/Requete_parametre.php");
+	}
 
-$arrayMois = array('1','2','3','4','5','6','7','8','9','10','11','12');
+	catch (Exception $e){
+		die('Erreur : ' . $e->getMessage());
+	}
 
-$datay = array();
+	$arrayMois = array('1','2','3','4','5','6','7','8','9','10','11','12');
 
-$req = $bdd->prepare("SELECT facture.Prix FROM facture,user_maison,utilisateur WHERE user_maison.ID_Maison = facture.ID_Maison AND utilisateur.ID_User = user_maison.ID_Maison AND utilisateur.ID_User = ? GROUP BY MONTH(facture.Date_facture) ASC");
-	$req->bindParam(1, $_SESSION['id']);
-	$req->execute();
+	$datay = array();
 
-while ($donnees = $req->fetch())
-{
-	$datay[] = $donnees['Prix'];
-}
+	$req = $bdd->prepare("SELECT facture.Prix FROM facture,user_maison,utilisateur WHERE user_maison.ID_Maison = facture.ID_Maison AND utilisateur.ID_User = user_maison.ID_Maison AND utilisateur.ID_User = ? GROUP BY MONTH(facture.Date_facture) ASC");
+		$req->bindParam(1, $_SESSION['id']);
+		$req->execute();
 
-$req->closeCursor();
+	while ($donnees = $req->fetch()){
+		$datay[] = $donnees['Prix'];
+	}
 
-/*$datay1 = array(2100,2150,1900,1700,1600,1400,1000,900,1100,1950,2100,2200);
-$datay2 = array(2200,2100,1850,1600,1400,1550,1100,850,1150,1950,2150,2300);
-$datay3 = array(2100,2150,1900,1700,1600,1550,1000,900,1100,1950,2000,2150);*/
+	if (empty($datay)){
+		header('Location: ../../Vues/Image/pas_de_data.png');
+		exit;
+	}
 
-// Setup the graph
-$graph = new Graph(600,500);
-$graph->SetScale("textlin");
+	else{
+	$req->closeCursor();
 
-$theme_class=new UniversalTheme;
+	// Setup the graph
+	$graph = new Graph(600,500);
+	$graph->SetScale("textlin");
 
-$graph->SetTheme($theme_class);
-$graph->img->SetAntiAliasing(false);
-$graph->title->Set('Graphique factures');
-$graph->SetBox(false);
+	$theme_class=new UniversalTheme;
 
-//$graph->SetMargin(40,20,36,83);
-$graph->SetMargin(50,20,36,83);
+	$graph->SetTheme($theme_class);
+	$graph->img->SetAntiAliasing(false);
+	$graph->title->Set('Graphique factures');
+	$graph->SetBox(false);
 
-$graph->img->SetAntiAliasing();
+	//$graph->SetMargin(40,20,36,83);
+	$graph->SetMargin(50,20,36,83);
 
-$graph->yaxis->HideZeroLabel();
-$graph->yaxis->HideLine(false);
-$graph->yaxis->HideTicks(false,false);
-$graph->yaxis->title->Set("Prix en €");
+	$graph->img->SetAntiAliasing();
 
-$graph->xgrid->Show();
-$graph->xgrid->SetLineStyle("solid");
-$graph->xaxis->SetTickLabels($arrayMois);
-$graph->xgrid->SetColor('#E3E3E3');
-$graph->xaxis->title->Set("Mois de l'année");
+	$graph->yaxis->HideZeroLabel();
+	$graph->yaxis->HideLine(false);
+	$graph->yaxis->HideTicks(false,false);
+	$graph->yaxis->title->Set("Prix en €");
 
-// Create the first line
-$p1 = new LinePlot($datay);
-$graph->Add($p1);
-$p1->SetColor("#6495ED");
-$p1->SetLegend('2019');
+	$graph->xgrid->Show();
+	$graph->xgrid->SetLineStyle("solid");
+	$graph->xaxis->SetTickLabels($arrayMois);
+	$graph->xgrid->SetColor('#E3E3E3');
+	$graph->xaxis->title->Set("Mois de l'année");
 
-// Create the second line
-/*$p2 = new LinePlot($datay2);
-$graph->Add($p2);
-$p2->SetColor("#B22222");
-$p2->SetLegend('2017');
+	// Create the first line
+	$p1 = new LinePlot($datay);
+	$graph->Add($p1);
+	$p1->SetColor("#6495ED");
+	$p1->SetLegend('2019');
 
-// Create the third line
-$p3 = new LinePlot($datay3);
-$graph->Add($p3);
-$p3->SetColor("#FF1493");
-$p3->SetLegend('2018');*/
+	// Create the second line
+	/*$p2 = new LinePlot($datay2);
+	$graph->Add($p2);
+	$p2->SetColor("#B22222");
+	$p2->SetLegend('2017');
 
-$graph->legend->SetFrameWeight(1);
+	// Create the third line
+	$p3 = new LinePlot($datay3);
+	$graph->Add($p3);
+	$p3->SetColor("#FF1493");
+	$p3->SetLegend('2018');*/
 
-// Output line
-$graph->Stroke();
+	$graph->legend->SetFrameWeight(1);
+
+	// Output line
+	$graph->Stroke();
+	}
 ?>
