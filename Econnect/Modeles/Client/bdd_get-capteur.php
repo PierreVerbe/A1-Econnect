37deb0ee67c7a4d326f7efb609f8863aab7b52e9
Yanis ISEP <yanis.kettou@isep.fr>
2019-06-07 10:12:07 +0200

    Gestion de la maison des capteurs et des actionneurs
    Y'a tout qui est adapté pour les sessions utilisateurs

--- Econnect/Modeles/Client/bdd_get-capteur.php
+++ Econnect/Modeles/Client/bdd_get-capteur.php
@@ -9,6 +9,9 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 
-	$capteur = $bdd->query("SELECT * FROM `capteur` INNER JOIN piece ON capteur.ID_Piece = piece.ID_Piece INNER JOIN user_maison ON piece.ID_Maison = user_maison.ID_Maison INNER JOIN utilisateur ON user_maison.ID_User = utilisateur.ID_User WHERE Adresse_email = \"pablo.grana@isep.fr\";");
+	$capteur = $bdd->prepare("SELECT * FROM `capteur` INNER JOIN piece ON capteur.ID_Piece = piece.ID_Piece INNER JOIN user_maison ON piece.ID_Maison = user_maison.ID_Maison INNER JOIN utilisateur ON user_maison.ID_User = utilisateur.ID_User WHERE utilisateur.ID_User = ?;");
+	$capteur->bindParam(1,$_SESSION['id']);
+	$capteur->execute();
+
 
 ?>
