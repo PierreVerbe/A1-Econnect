394dc5d116f4dd81ba985d6a30328975b52ea38f
Pierre Verbe <pierre.verbe@free.fr>
2019-06-07 10:53:16 +0200

    slider mode maison

--- Econnect/Modeles/Client/Requete_banniere_slider_nom.php
+++ Econnect/Modeles/Client/Requete_banniere_slider_nom.php
@@ -18,7 +18,7 @@
 		}
 
 	//Récupération du mode de la maison
-	$req = $bdd->prepare("SELECT maison.Mode_maison FROM utilisateur, maison, user_maison WHERE user_maison.ID_User = utilisateur.ID_User AND user_maison.ID_Maison = maison.ID_Maison AND utilisateur.ID_User = ?");
+	$req = $bdd->prepare("SELECT maison.Mode_maison FROM utilisateur, maison, user_maison WHERE user_maison.ID_User = utilisateur.ID_User AND user_maison.ID_Maison = maison.ID_Maison AND utilisateur.ID_User = ? ORDER BY maison.ID_Maison ASC LIMIT 1");
 	$req->bindParam(1, $_SESSION['id']);
 	$req->execute();
 			  	
