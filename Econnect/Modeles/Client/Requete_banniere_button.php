78dc9bdd3bd59da9736cc41e0090b8482d2ac294
Pierre Verbe <pierre.verbe@free.fr>
2019-06-07 10:49:45 +0200

    requete changement mode maison bdd

--- Econnect/Modeles/Client/Requete_banniere_button.php
+++ Econnect/Modeles/Client/Requete_banniere_button.php
@@ -1,4 +1,6 @@
 <?php
+	session_start();
+
 	try{
 		include ("../../Modeles/Requete_parametre.php");
 	}
@@ -8,39 +10,23 @@
 
 	if (isset($_POST['postid'])){
 		$mode = $_POST['postid'];
-		$idUser = $_POST['postuser']
-	
-	$reqa = $bdd->prepare("INSERT INTO test(id_user, mode) VALUES (?, ?)");
-	$reqa->bindParam(1, $idUser);
-	$reqa->bindParam(2, $mode);
-	$reqa->execute();
 
-	//Récupration de l'ID maison
-	$req = $bdd->prepare("SELECT * FROM utilisateur, maison, user_maison WHERE user_maison.ID_User = utilisateur.ID_User AND user_maison.ID_Maison = maison.ID_Maison AND utilisateur.ID_User = ? LIMIT 1");
-	$req->bindParam(1, $idUser);
-	$req->execute();
+		//Récupration de l'ID de la premiere maison
+		$req = $bdd->prepare("SELECT maison.ID_Maison FROM utilisateur, maison, user_maison WHERE  user_maison.ID_Maison = maison.ID_Maison AND user_maison.ID_User = utilisateur.ID_User AND utilisateur.ID_User = ? ORDER BY maison.ID_Maison ASC LIMIT 1");
+		$req->bindParam(1, $_SESSION['id']);
+		$req->execute();
 
-	//Récupration de l'ID maison
-	//$req = $bdd->query('SELECT maison.ID_maison FROM utilisateur, maison, user_maison WHERE user_maison.ID_User = utilisateur.ID_User AND user_maison.ID_Maison = maison.ID_Maison AND utilisateur.ID_User = 1');
-
-	//Pour récupérer la dernière maison de l'utilisateur 
-	while ($donnees = $req->fetch()){
-		$ID_maison = $donnees['ID_maison'];
-		echo($ID_maison);
-		?> <script>alert("ICI");</script> <?php
-	}
-
-	//Pour récupérer la première maison de l'utilisateur 
-	// if ($donnees = $req->fetch())$ID_maison = $donnees['ID_maison']; 
-		
-	//changement du mode de la maison
-	$reqi = $bdd->prepare('UPDATE maison SET Mode_maison = "?" WHERE ID_maison = ?');
-	$reqi->bindParam(1, $mode);
-	$reqi->bindParam(2, $ID_maison);
-	$reqi->execute();
-
-	//$bdd->query('UPDATE maison SET Mode_maison = "'.$mode.'" WHERE ID_maison = "'.$ID_maison.'"');
+		while ($donnees = $req->fetch()){
+			$ID_maison = $donnees['ID_Maison'];
+		}
+			
+		//changement du mode de la maison
+		$reqi = $bdd->prepare('UPDATE maison SET Mode_maison = ? WHERE ID_maison = ?');
+		$reqi->bindParam(1, $mode);
+		$reqi->bindParam(2, $ID_maison);
+		$reqi->execute();
 	}
 
 	$req->closeCursor();
+	$reqi->closeCursor();
 ?>
--- Econnect/Vues/Client/javascript/button_client.js
+++ Econnect/Vues/Client/javascript/button_client.js
@@ -1,34 +1,34 @@
-function ecoMode(id_user_tkt){ 
+function ecoMode(){ 
  	var mode = document.getElementById('bouton_eco').value;	
  	alert('Vous passez en mode : ' + mode);
-	$.post("../../Modeles/Client/Requete_banniere_button.php", {postid: mode, postuser: id_user_tkt},
+	$.post("../../Modeles/Client/Requete_banniere_button.php", {postid: mode},
 	function(data){
 		return;
 	});
 }
 
-function sortieMode(id_user_tkt){ 
+function sortieMode(){ 
  	var mode = document.getElementById('bouton_sortie').value;	
  	alert('Vous passez en mode : ' + mode);
-	$.post("../../Modeles/Client/Requete_banniere_button.php", {postid: mode, postuser: id_user_tkt},
+	$.post("../../Modeles/Client/Requete_banniere_button.php", {postid: mode},
 	function(data){
 		return;
 	});
 }
 
-function confortMode(id_user_tkt){ 
+function confortMode(){ 
  	var mode = document.getElementById('bouton_confort').value;	
  	alert('Vous passez en mode : ' + mode);
-	$.post("../../Modeles/Client/Requete_banniere_button.php", {postid: mode, postuser: id_user_tkt},
+	$.post("../../Modeles/Client/Requete_banniere_button.php", {postid: mode},
 	function(data){
 		return;
 	});
 }
 
-function hibernationMode(id_user_tkt){ 
+function hibernationMode(){ 
  	var mode = document.getElementById('bouton_hibernation').value;	
  	alert('Vous passez en mode : ' + mode);
-	$.post("../../Modeles/Client/Requete_banniere_button.php", {postid: mode, postuser: id_user_tkt},
+	$.post("../../Modeles/Client/Requete_banniere_button.php", {postid: mode},
 	function(data){
 		return;
 	});
